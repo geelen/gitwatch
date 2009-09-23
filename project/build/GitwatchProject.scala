d197608e5265d290fa81156e30485e523d62e933
Glen Maddern <glen83@gmail.com>
2009-09-23 23:59:18 +1000

    Added jgit and scapps deps. Scapps needs to be built and publish-local.

--- project/build/GitwatchProject.scala
+++ project/build/GitwatchProject.scala
@@ -4,4 +4,10 @@ class GitwatchProject(info: ProjectInfo) extends DefaultWebProject(info) {
   import BasicScalaProject._
 
   val specs = "org.scala-tools.testing" % "specs" % "1.6.0"
+
+  val jgitRepo = "jgit-snapshot-repository" at "http://egit.googlecode.com/svn/maven/"
+
+  val jgit = "org.spearce" % "jgit" % "0.5.0"
+
+  val scapps = "scapps" % "scapps" % "0.1"
 }
