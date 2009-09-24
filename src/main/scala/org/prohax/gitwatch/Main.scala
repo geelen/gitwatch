242a0ff4de5d1b7bdde3d72d0bc547074762bcfd
Glen Maddern <glen83@gmail.com>
2009-09-24 13:02:41 +1000

    RhinoScriptEngine working in sbt under linux ok without any dep changes

--- src/main/scala/org/prohax/gitwatch/Main.scala
+++ src/main/scala/org/prohax/gitwatch/Main.scala
@@ -1,9 +1,14 @@
 package org.prohax.gitwatch
 
+import com.sun.script.javascript.RhinoScriptEngine
+
 object Main {
   def main(args: Array[String]) {
     args match {
       case Array() => println("Usage: run GIT_DIR [OUTPUT_FILE]")
+      case Array("js") => {
+        new RhinoScriptEngine().eval("println(5)")
+      }
       case Array(git) => println(GitProHax.run(git,"refs/heads/master"))
     }
   }
